0053a28c1fab26937177068405f3170b823a056a
SKATTKOOKIE <skattkookie@gmail.com>
2025-12-04 16:37:14 +0000

    stuff from work with hiding navbar and adding shutdown button

--- src/core/UIComponent.h
+++ src/core/UIComponent.h
@@ -1,8 +1,15 @@
 #pragma once
 
+struct GLFWwindow;
+
 class UIComponent
 {
 public:
     virtual ~UIComponent() = default;
     virtual void Render() = 0;
+
+    void SetWindow(GLFWwindow *win) { window = win; }
+
+protected:
+    GLFWwindow *window = nullptr;
 };
